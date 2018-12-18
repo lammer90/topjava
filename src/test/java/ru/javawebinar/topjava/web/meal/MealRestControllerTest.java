68ac69e1093fa8993855e09b5df89cb7338eedd3
Dmitriy <lammer.90@yandex.ru>
2018-12-18 15:35:00 +0300

    Lesson7_HW_0.9

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -2,9 +2,17 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,9 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static ru.javawebinar.topjava.MealTestData.MEAL1;
 import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
 import static ru.javawebinar.topjava.MealTestData.MEALS;
-import static ru.javawebinar.topjava.UserTestData.ADMIN;
-import static ru.javawebinar.topjava.UserTestData.USER;
-import static ru.javawebinar.topjava.UserTestData.contentJson;
+import static ru.javawebinar.topjava.UserTestData.*;
 import static ru.javawebinar.topjava.web.meal.MealRestController.REST_URL;
 
 class MealRestControllerTest extends AbstractControllerTest {
@@ -29,7 +35,13 @@ class MealRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    void testDelete() {
+    void testDelete() throws Exception {
+        List<Meal> newMeals = new ArrayList<>(MEALS);
+        newMeals.remove(MEAL1);
+        mockMvc.perform(delete(REST_URL + "/" + MEAL1_ID))
+                .andDo(print())
+                .andExpect(status().isOk());
+        MealTestData.assertMatch(mealService.getAll(SecurityUtil.authUserId()), newMeals);
     }
 
     @Test
@@ -38,7 +50,7 @@ class MealRestControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(contentJson(MEALS.toArray(new Meal[0])));
+                .andExpect(contentJson(MealsUtil.getWithExcess(MEALS, 2000).toArray(new MealTo[0])));
     }
 
     @Test
