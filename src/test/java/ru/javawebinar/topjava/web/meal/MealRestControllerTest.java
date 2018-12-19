d9b16caa89020ae5230799ffbecf6b3cab7d7962
Dmitriy <lammer.90@yandex.ru>
2018-12-19 10:53:32 +0300

    Lesson7_HW

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -2,24 +2,29 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 import ru.javawebinar.topjava.web.SecurityUtil;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static ru.javawebinar.topjava.MealTestData.MEAL1;
-import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
-import static ru.javawebinar.topjava.MealTestData.MEALS;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
 import static ru.javawebinar.topjava.web.meal.MealRestController.REST_URL;
 
@@ -54,14 +59,34 @@ class MealRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    void testCreateNew() {
+    void testCreateNew() throws Exception {
+        Meal expected = MealTestData.getCreated();
+        mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(expected));
     }
 
     @Test
-    void testUpdate() {
+    void testUpdate() throws Exception  {
+        Meal expected = MealTestData.getUpdated();
+        mockMvc.perform(post(REST_URL + "/" + MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected)))
+                .andExpect(status().isOk())
+                .andDo(print());
+        MealTestData.assertMatch(mealService.get(MEAL1_ID, SecurityUtil.authUserId()), expected);
     }
 
     @Test
-    void testGetBetween() {
+    void testGetBetween() throws Exception {
+        mockMvc.perform(get(REST_URL + "/filter?startDate=2015-05-31&startTime=20:00:00&endDate=2015-05-31&endTime=20:00:00"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(Collections.singletonList(MEAL6).toArray(new Meal[0])));
     }
 }
